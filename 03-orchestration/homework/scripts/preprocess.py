feb956a653e5911283eb938418f2ca779e74cf1c
Ubuntu <azureuser@azurevm.avifl3mjfbbupj4dyzzfdf3thb.ux.internal.cloudapp.net>
2025-06-18 01:15:53 +0000

    added comments

--- 03-orchestration/homework/scripts/preprocess.py
+++ 03-orchestration/homework/scripts/preprocess.py
@@ -19,21 +19,26 @@ def create_X(df, dv=None):
     return X, dv
 
 if __name__ == "__main__":
+    # Define arguments for command line initilization
     parser = argparse.ArgumentParser()
     parser.add_argument('--train_input', type=str, required=True)
     parser.add_argument('--val_input', type=str, required=True)
     parser.add_argument('--output_dir', type=str, required=True)
     args = parser.parse_args()
 
+    # Create directory to output files to
     output_dir = Path(args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
 
+    # get training and validation data
     df_train = pd.read_parquet(args.train_input)
     df_val = pd.read_parquet(args.val_input)
 
+    # Transform and train data sets
     X_train, dv = create_X(df_train)
     X_val, _ = create_X(df_val, dv)
 
+    # Output data into folder
     with open(output_dir / "X_train.pkl", "wb") as f_out:
         pickle.dump(X_train, f_out)
 

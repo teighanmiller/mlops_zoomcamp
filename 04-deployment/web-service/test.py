0f2381b3efd890d0eb3a494e4542a5b0f46a15c5
Ubuntu <azureuser@azurevm.avifl3mjfbbupj4dyzzfdf3thb.ux.internal.cloudapp.net>
2025-06-18 19:10:49 +0000

    changed test steps

--- 04-deployment/web-service/test.py
+++ 04-deployment/web-service/test.py
@@ -1,4 +1,4 @@
-import predict
+import requests
 
 ride = {
     "PULocationID": 10,
@@ -6,6 +6,7 @@ ride = {
     'trip_distance': 40
 }
 
-features = predict.prepare_features(ride)
-pred = predict.predict(features)
-print(pred)
+url='http://127.0.0.1:9696/predict'
+response = requests.post(url, json=ride)
+
+print(response.json())
